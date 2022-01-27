e97cdc54791ac62dce985a547a9b3bb4478f415d
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-27 14:06:09 +0000

    setup redux store

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
