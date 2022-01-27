5219235dbd78939f27e241f4e02ff34340953f70
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-27 18:33:05 +0000

    Feature component & images

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, Box } from "@mui/material";
 import * as React from "react";
 
 interface FeatureCardProps {
@@ -22,14 +22,38 @@ const FeatureCard = ({ first, second }: FeatureCardProps) => {
 const Features = () => {
   return (
     <Container>
-      <Grid>
-        <Typography variant="h5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
-          vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus, a
-          maximus nisl sapien vitae justo.
-        </Typography>
-        <img src="http://placehold.it/300x300" alt="placeholder" />
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        spacing={12}
+      >
+        <Grid item md={6}>
+          <Typography variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
+            vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus,
+            a maximus nisl sapien vitae justo.
+          </Typography>
+        </Grid>
+        <Grid item md={4}>
+          <Box sx={{ maxWidth: "100%" }}>
+            <img src="/feature-1.svg" alt="feature one" width="100%" />
+          </Box>
+        </Grid>
+        <Grid item md={4}>
+          <Box sx={{ maxWidth: "100%" }}>
+            <img src="/feature-2.svg" alt="feature two" width="100%" />
+          </Box>
+        </Grid>
+        <Grid item md={8}>
+          <Typography variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
+            vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus,
+            a maximus nisl sapien vitae justo.
+          </Typography>
+        </Grid>
       </Grid>
     </Container>
   );
--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import MOCK_DATA from './mock';
-import { CardMedia,CardContent,Typography,Card } from '@mui/material';
-import { StyledCard,StyledTypography,StyledBox} from './trending.style';
+import React from "react";
+import MOCK_DATA from "./mock";
+import { CardMedia, CardContent, Typography, Container } from "@mui/material";
+import { StyledCard, StyledTypography, StyledBox } from "./trending.style";
 
 const Trending = () => {
-  return <>
-          <h3>TRENDING</h3>
-          <StyledBox>
-          {MOCK_DATA.map(item=>{
-              return <StyledCard key={item.article_id} elevation={0}>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={item.article_img}
-                    alt="img"
-                  />
-                  <CardContent >
-                      <h4>{item.title}</h4>
-                    <StyledTypography variant="body2" color="text.secondary">
-                        {item.article_body}
-                    </StyledTypography>
-                    <Typography variant="caption">
-                        {item.created_at}
-                    </Typography>
-                  </CardContent>
-                </StyledCard>
-          })}
-          </StyledBox>
-  </>;
+  return (
+    <Container width="100%">
+      <Typography variant="h6">
+        <b>Trending...</b>{" "}
+      </Typography>
+      <StyledBox>
+        {MOCK_DATA.map((item) => {
+          return (
+            <StyledCard key={item.article_id} elevation={0}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.article_img}
+                alt="img"
+              />
+              <CardContent>
+                <h4>{item.title}</h4>
+                <StyledTypography variant="body2" color="text.secondary">
+                  {item.article_body}
+                </StyledTypography>
+                <Typography variant="caption">{item.created_at}</Typography>
+              </CardContent>
+            </StyledCard>
+          );
+        })}
+      </StyledBox>
+    </Container>
+  );
 };
 
 export default Trending;
