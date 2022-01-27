02b4b09e5c31fc1ef9bd716e27de50b4cd639aeb
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-27 18:58:06 +0000

    Feature component final

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,78 +1,68 @@
 import { Container, Grid, Typography, Box } from "@mui/material";
+import type { ResponsiveStyleValue } from "@mui/system/styleFunctionSx";
 import * as React from "react";
 
-interface FeatureCardProps {
-  first: React.ReactNode;
-  second: React.ReactNode;
+interface FeatureImageProps {
+  src: string;
+  alt: string;
+  width?: string;
 }
 
-const FeatureCard = ({ first, second }: FeatureCardProps) => {
+interface FeatureTextProps {
+  children: React.ReactNode[] | React.ReactNode;
+  id?: string;
+  textAlign?: "justify" | "center" | "left" | "right";
+}
+
+const FeatureText = ({ children, textAlign = "left" }: FeatureTextProps) => {
   return (
-    <Grid container justifyContent="center">
-      <Grid item md={6}>
-        {first}
-      </Grid>
-      <Grid item md={6}>
-        {second}
-      </Grid>
-    </Grid>
+    <Box width="100%" sx={{ textAlign }}>
+      <Typography variant="h6">{children}</Typography>
+    </Box>
+  );
+};
+
+const FeatureImage = ({ src, alt, width = "100%" }: FeatureImageProps) => {
+  return (
+    <Box sx={{ maxWidth: width }}>
+      <img src={src} width={width} alt={alt} />
+    </Box>
   );
 };
 
 const Features = () => {
   return (
-    <Container>
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-between"
-        spacing={12}
-      >
-        <Grid item md={6}>
-          <Typography variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
-            vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus,
-            a maximus nisl sapien vitae justo.
-          </Typography>
-        </Grid>
-        <Grid item md={4}>
-          <Box sx={{ maxWidth: "100%" }}>
-            <img src="/feature-1.svg" alt="feature one" width="100%" />
-          </Box>
-        </Grid>
-        <Grid item md={4}>
-          <Box sx={{ maxWidth: "100%" }}>
-            <img src="/feature-2.svg" alt="feature two" width="100%" />
-          </Box>
-        </Grid>
-        <Grid item md={8}>
-          <Typography variant="h6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
-            vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus,
-            a maximus nisl sapien vitae justo.
-          </Typography>
-        </Grid>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      spacing={12}
+      width="100%"
+    >
+      <Grid item md={6}>
+        <FeatureText textAlign="justify">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
+          vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus, a
+          maximus nisl sapien vitae justo.
+        </FeatureText>
       </Grid>
-    </Container>
+      <Grid item md={4}>
+        <FeatureImage src="/feature-1.svg" alt="feature one" />
+      </Grid>
+      <Grid item md={4}>
+        <FeatureImage src="/feature-2.svg" alt="feature two" width="100%" />
+      </Grid>
+      <Grid item md={8}>
+        <FeatureText textAlign="justify">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
+          vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus, a
+          maximus nisl sapien vitae justo.
+        </FeatureText>
+      </Grid>
+    </Grid>
   );
 };
 
 export default Features;
-
-/*
-<Typography variant="h5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          feugiat purus urna, sit amet dapibus odio maximus suscipit. Nam
-          vestibulum, sem ut facilisis ullamcorper, lorem risus mollis risus, a
-          maximus nisl sapien vitae justo. Donec tempor mi odio, sit amet
-          accumsan elit cursus et. Etiam metus mi, ultrices sed velit ut,
-          ullamcorper efficitur augue. Duis eu massa quis sem commodo sagittis.
-          Suspendisse lorem orci, accumsan nec consequat nec, tincidunt nec sem.
-          Aliquam eu nibh ligula. Duis convallis, urna at pellentesque interdum,
-          quam massa fermentum mi, et ornare neque ex scelerisque dui.
-          Pellentesque ac lectus sodales, interdum nisi at, aliquet libero.{' '}
-        </Typography>
-        <img src="http://placehold.it/300x300" alt="placeholder" />
-*/
