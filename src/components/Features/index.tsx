3f66d37abac3341fe2c4ad04082393229d8943c3
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-27 17:11:42 +0000

    Features component / card

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,5 +1,40 @@
+import { Container, Grid } from '@mui/material';
 import * as React from 'react';
 
-const Features = () => {};
+interface FeatureProps {
+  children: React.ReactNode[];
+}
+
+interface FeatureCardProps {
+  first: React.ReactNode;
+  second: React.ReactNode;
+}
+
+const FeatureCard = ({ first, second }: FeatureCardProps) => {
+  return (
+    <Grid container justifyContent="center">
+      <Grid item md={6}>
+        {first}
+      </Grid>
+      <Grid item md={6}>
+        {second}
+      </Grid>
+    </Grid>
+  );
+};
+
+const Features = ({ children }: FeatureProps) => {
+  return (
+    <Container>
+      {children.map((child, index) => {
+        if (index % 2 === 0) {
+          return <FeatureCard first={child} second={children[index + 1]} />;
+        } else {
+          return null;
+        }
+      })}
+    </Container>
+  );
+};
 
 export default Features;
