22f7db37fb9ea8aca65efcb65a7436892fc2b791
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-01-28 10:14:34 +0000

    palette styling

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,14 +9,32 @@ export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     background: {
+      default: '#eeeeee',
       paper: '#f2f2f2',
     },
     text: {
-      primary: '#111111',
+      primary: '#111',
+      secondary: '#111',
+    },
+    primary: {
+      main: '#4527a0',
+    },
+    secondary: {
+      main: '#f50057',
     },
   },
   typography: {
     fontFamily: ['Manrope', 'Roboto', 'sans-serif'].join(','),
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#f2f2f2',
+          color: '#111',
+        },
+      },
+    },
   },
 });
 
@@ -24,13 +42,35 @@ export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
     background: {
+      default: 'rgba(0,0,0,0.27)',
       paper: '#222',
     },
     text: {
       primary: '#fff',
+      secondary: '#fff',
+    },
+    primary: {
+      main: '#c6ff00',
+      light: '#dce775',
+    },
+    secondary: {
+      main: '#7c4dff',
+      light: '#b388ff',
     },
   },
   typography: {
     fontFamily: ['Manrope', 'Roboto', 'sans-serif'].join(','),
   },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#222',
+          color: '#fff',
+        },
+      },
+    },
+  },
 });
+
+// rgba(0, 0, 0, 0.87)
